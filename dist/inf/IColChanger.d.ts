a081d907c6cd67deea5706827dbfc4324b6e6c0b
ytrgmj@gmail.com <ytrgmj@gmail.com>
2025-07-18 08:11:22 +0800

    0918

--- dist/inf/IColChanger.d.ts
+++ dist/inf/IColChanger.d.ts
@@ -1,4 +1,13 @@
 export default interface IColChanger {
-    srcCol: string;
+    /**
+     * srcCol 和 targetcol是一对，
+     */
+    srcCol?: string;
     targetCol?: string;
+    /**
+     * 直接进行转换
+     * @param data1
+     * @param data2
+     */
+    change?(src: any, target: any): any;
 }
